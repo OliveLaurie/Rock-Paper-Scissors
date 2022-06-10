6b06b48196de3a52129f7e610b4ab8eadef805ef
OliveLaurie <laureijsolivia@gmail.com>
2022-06-09 20:43:30 -0400

    Create function to display winner.

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 
 const myArray = ['rock', 'paper', 'scissors']
 
-computerPlay()
+let computerSelection = computerPlay()
+let playerSelection = prompt('Enter your choice: ')
+
+gameRound(computerSelection, playerSelection)
 
 function computerPlay(){
     let index = Math.floor(Math.random()*3)
     console.log(index)
-    console.log(myArray[index])
+    return myArray[index]
+}
+
+function gameRound(computer, player){
+    
+    let result = document.querySelector('h3')
+    
+    if((player == 'rock' && computer == 'scissors') || (player == 'paper' && computer == 'rock') || (player == 'scissors' && computer == 'paper')){
+        result.textContent += 'You win!'
+        console.log('You win!')
+    }
+    else if(player === computer){
+        result.textContent += 'Tie!'
+        console.log("Tie!")
+    }
+    else{
+        result.textContent += 'Computer wins!'
+        console.log("Computer wins!")
+    }
 }
 
